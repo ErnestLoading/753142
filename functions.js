ddfc9cd32f7f30b52ed183e5afc25722fe8008de
ErnestLoading <144675548+ErnestLoading@users.noreply.github.com>
2024-12-06 15:44:49 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,16 @@
+const themeToggleBtn = document.getElementById("theme-toggle");
+const body= document.body;
+
+const updateButton = () => {
+  if (body.classList.contains("dark-mode")) {
+    themeToggleBtn.textContent="Šviesus režimas";
+  } else {
+    themeToggleBtn.textContent="Tamsus režimas";
+  }
+};
+
+themeToggleBtn.addEventListener("click", () =>{
+  body.classList.toggle("dark-mode"); // Perjungia klasę "dark-mode"
+  updateButton();
+});
+updateButton();
